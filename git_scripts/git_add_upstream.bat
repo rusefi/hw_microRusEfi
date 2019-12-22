5c7fba71e0b17024f46b818825392716c05da1a2
rusefi <rusefillc@gmail.com>
2019-12-22 00:42:54 -0500

    git magic

--- git_scripts/git_add_upstream.bat
+++ git_scripts/git_add_upstream.bat
@@ -1,3 +1,7 @@
 git remote -v
 git remote add upstream https://github.com/rusefi/hw_microRusEfi.git
+
+rem The heros, in alpabetical order
+git remote add DonaldBecker https://github.com/DonaldBecker/hw_microRusEfi
+
 git remote -v
