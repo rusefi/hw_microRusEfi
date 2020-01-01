da8bba89d8ec5b24b77dfe44d1e66a57514081d4
rusefi <rusefillc@gmail.com>
2020-01-01 06:46:06 -0500

    semi-automatic, half-manual placement file progress

--- gerbers/kicadPos2jlcpcb.bat
+++ gerbers/kicadPos2jlcpcb.bat
@@ -3,7 +3,7 @@ rem This script takes care only of the mechanical part of the transformation - t
 rem 
 
 set inputFile=microRusEfi-top-pos.csv
-set outputFile=rusEFI-Micro_R0.4.5-cpl.csv
+set outputFile=rusEFI-Micro_R0.4.7-cpl.csv
 
 echo "Translating grom %inputFile% to %outputFile%"
 
