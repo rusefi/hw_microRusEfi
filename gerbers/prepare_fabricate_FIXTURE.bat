b349067fa9aaf8c3525fcd0b8e4b70d8574a8d8f
jharvey <github@jaredharvey.com>
2020-05-14 07:42:59 -0400

    R0.1

---
+++ gerbers/prepare_fabricate_FIXTURE.bat
@@ -0,0 +1,14 @@
+
+set posInputFile=zMRETestFixture_R0.1/zMRETestFixture-top-pos.csv
+set posOutputFile=zMRETestFixture_R0.1/zMRETestFixture-cpl_initial.csv
+set lib_path=../rusefi_lib_external
+
+rem set bomInputFile=../microRusEfi.csv
+rem set bomOutputFile=0.4.9/rusEFI-Micro_R0.4.9-bom.csv
+
+mkdir zMRETestFixture_R0.1
+
+call %lib_path%/Fabrication/jlc_prepare_position.bat %posInputFile% %posOutputFile% %lib_path%
+
+
+rem call %lib_path%/Fabrication/jlc_prepare_bom.bat      %bomInputFile% %bomOutputFile% %lib_path%
