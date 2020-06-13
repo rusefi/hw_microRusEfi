313b4028707878aaf9489456d52d41c9a5841235
rusefi <rusefillc@gmail.com>
2020-06-13 15:04:55 -0400

    ibom & fresh libraries

--- gerbers/gen_ibom.bat
+++ gerbers/gen_ibom.bat
@@ -1,6 +1,7 @@
 
-set version=0.5.0
+set version=R0.5.1
 set dest_dir=gerbers/%version%
+mkdir %dest_dir%
 
 set kicad_path=c:\progra~1\KiCad
 set ibom_cmd=%kicad_path%\bin\python.exe InteractiveHtmlBom\InteractiveHtmlBom\generate_interactive_bom.py --no-browser --name-format %%f_%version% --dest-dir %dest_dir%
