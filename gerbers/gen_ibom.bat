d01f4b689de78054e4128a4baf928d9d86735651
rusefillc <sdfsdfqsf2334234234>
2022-05-19 20:40:01 -0400

    progress kicad 6.0 for 0.6.0

--- gerbers/gen_ibom.bat
+++ gerbers/gen_ibom.bat
@@ -3,6 +3,6 @@ set version=R0.6.0
 set dest_dir=gerbers/%version%
 mkdir %dest_dir%
 
-set kicad_path=c:\progra~1\KiCad
+set kicad_path=c:\progra~1\KiCad\6.0
 set ibom_cmd=%kicad_path%\bin\python.exe InteractiveHtmlBom\InteractiveHtmlBom\generate_interactive_bom.py --no-browser --name-format %%f_%version% --dest-dir %dest_dir%
 %ibom_cmd% ../micro_rusEFI.kicad_pcb
